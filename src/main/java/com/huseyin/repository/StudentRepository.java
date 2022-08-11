ba22a1241b4c4ec423093bdb47c92fbabc57a85a
huseyingurel1 <huseyingurel27@gmail.com>
2022-08-11 11:53:27 +0300

    updated login register

--- src/main/java/com/huseyin/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.huseyin.repository;
-//
-//import com.huseyin.model.Student;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface StudentRepository extends JpaRepository<Student, Long > {
-//}
